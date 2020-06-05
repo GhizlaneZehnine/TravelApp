5c071831cc432cdfc53c04c33ff1066cfb042356
Othmane-zarai <57667825+Othmane-zarai@users.noreply.github.com>
2020-06-05 17:17:36 +0100

    Translation-Commit

--- Application/app/src/main/java/com/example/travelapp/Translation_Activity.java
+++ Application/app/src/main/java/com/example/travelapp/Translation_Activity.java
@@ -1,6 +1,8 @@
 package com.example.travelapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -8,17 +10,22 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
 import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
 import com.google.firebase.ml.naturallanguage.languageid.FirebaseLanguageIdentification;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
 import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
 public class Translation_Activity extends AppCompatActivity {
     static String language_name;
     public final String[] language_names = {"Afrikaans","Arabic","Belarusian","Bulgarian","Czech","German",
@@ -27,12 +34,14 @@ public class Translation_Activity extends AppCompatActivity {
     public EditText input_text;
     public TextView output_text;
     public Button translate;
-    public String source_text;
+    public  String source_text;
     private  Spinner spinner;
+    public ArrayList<String> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_translation_main);
         input_text = findViewById(R.id.input_text);
         output_text = findViewById(R.id.output_text);
@@ -43,7 +52,7 @@ public class Translation_Activity extends AppCompatActivity {
         ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,language_names);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
-        spinner.setAdapter(aa);;
+        spinner.setAdapter(aa);
         translate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,7 +62,13 @@ public class Translation_Activity extends AppCompatActivity {
     }
 
     public void indentifyLanguage() {
-        source_text = input_text.getText().toString();
+        System.out.println(result.get(0));
+        if(!input_text.getText().toString().matches("")){
+            source_text = input_text.getText().toString();
+        }
+        else{
+            source_text =  result.get(0);
+        }
         FirebaseLanguageIdentification identifier = FirebaseNaturalLanguage.getInstance().getLanguageIdentification();
         identifier.identifyLanguage(source_text).addOnSuccessListener(new OnSuccessListener<String>() {
             @Override
@@ -63,6 +78,32 @@ public class Translation_Activity extends AppCompatActivity {
             }
         });
     }
+
+    public void getSpeechInput(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent,10);
+        }
+        else{
+            Toast.makeText(this,"Your Device don't support Speech Input",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode){
+            case 10:
+                if(resultCode == RESULT_OK && data != null){
+                    result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    input_text.setText(result.get(0));
+                }
+                break;
+        }
+    }
+
     public class SpinnerActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id){
